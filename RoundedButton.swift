2b3781a5084ce35d6cfd112170cd67fcf971f0a8
Mahesh Cheliya <maheshsheliya@gmail.com>
2021-06-15 12:13:49 -0400

    Update RoundedButton.swift

--- RoundedButton.swift
+++ RoundedButton.swift
@@ -59,7 +59,6 @@ class RoundedButton:UIButton {
         
         if imageView != nil {
             imageEdgeInsets = UIEdgeInsets(top: 5, left: (bounds.width - 35), bottom: 5, right: 5)
-//            titleEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: (imageView?.frame.width)!)
         }
         
         
